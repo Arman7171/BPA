04ea7d0c7d6b9bf8d396dbe780ef5fed95716ed8
Arman7171 <armvar774@gmail.com>
2020-11-19 22:00:33 +0400

    my project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './Components/Login'
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+
+
+export default App;
